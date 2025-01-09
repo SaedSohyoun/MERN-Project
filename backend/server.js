7df694b65308dbaeabde42fbf980f2445ddb879f
SaedSohyoun <9011518@student.zadkine.nl>
2025-01-09 21:38:29 +0100

    112

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path, { resolve } from 'path'
 import { connectDB } from './config/db.js';
 import productRoutes from "./routes/Product.route.js";
 
@@ -9,9 +10,19 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+const__dirname = path,resolve();
+
 app.use(express.json());
 
 app.use("/api/products", productRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     connectDB();
